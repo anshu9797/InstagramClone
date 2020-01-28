1e4e464bbbd68144912b1d5629d1d0d100b13e2f
anshu97 <anshumanghosh56@gmail.com>
2020-01-28 12:22:59 +0530

    logout code

--- app/src/main/java/com/example/instagramclone/WelcomeActivity.java
+++ app/src/main/java/com/example/instagramclone/WelcomeActivity.java
@@ -3,6 +3,10 @@ package com.example.instagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 public class WelcomeActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        TextView txtWelcome = findViewById(R.id.txtWelcome);
+        txtWelcome.setText("Welcome " + ParseUser.getCurrentUser().get("username"));
+
+        findViewById(R.id.btnLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                finish();
+            }
+        });
     }
 }
